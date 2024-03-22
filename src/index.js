211b33e8347a31323cc53b31e5b3f73c57532422
shihabrafio <aimshihabuddinkhan@gmail.com>
2024-03-23 01:07:31 +0600

    Add login and signup functionality to the app

---
+++ src/index.js
@@ -0,0 +1,53 @@
+const express = require("express")
+const app= express()
+const path= require("path")
+const hbs=require("hbs")
+const port = process.env.PORT || 3000
+const templatePath =path.join(__dirname ,'../templates')
+const collection = require("./mongodb")
+
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+app.set("view engine","hbs")
+app.set("views",templatePath)
+
+
+
+app.get("/",(req,res)=>{
+    res.render("login")
+})
+
+app.get("/signup",(req,res)=>{
+    res.render("signup")
+})
+
+app.post("/login", async(req,res)=>{
+    try{
+        const check = await collection.findOne({name:req.body.name})
+        if (check.password ===req.body.password){
+            res.render("home")
+        }
+        else{
+            res.send("Wrong Password")
+        }
+    }
+    catch{
+        res.send("Wrong Details")
+    }
+
+})
+
+app.post("/signup", async(req,res)=>{
+    const data={
+        name:req.body.name,
+        password:req.body.password
+    }
+
+    await collection.insertMany([data])
+
+    res.render("home")
+})
+
+app.listen(port,()=>{
+    console.log("port connected")
+})
